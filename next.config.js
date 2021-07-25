0503ec475d9343413da05d7d3106c62f335b9eae
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-07-25 16:34:32 +0600

    some change

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
   env: {
     PUBLIC_URL: 'https://safe-earth-37306.herokuapp.com/api',
     PUBLIC_URL_NODEJS: 'https://password-reset-all.herokuapp.com/api',
-    
-    
+
   },
 }
