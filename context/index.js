dca551df8cf8c55887862fceb0ffd9d7fdf5bdf1
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-07-27 15:54:59 +0600

    final

--- context/index.js
+++ context/index.js
@@ -30,12 +30,7 @@ useEffect(() => {
           
         }
         else {
-            axios.interceptors.request.use(function (config) {
-              
-                config.headers.Authorization = "Bearer " + token;
-                
-                return config;
-            });
+           
             setReady(true)
            
         }
